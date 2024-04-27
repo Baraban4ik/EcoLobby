97bbf3377e4749f25bdc9f018c60dcb5041702ac
Барабан4ик <vovapetrenko890@gmail.com>
2024-04-28 04:53:02 +0500

    Fix GSit move bug

--- src/main/java/me/baraban4ik/ecolobby/listeners/WorldListener.java
+++ src/main/java/me/baraban4ik/ecolobby/listeners/WorldListener.java
@@ -1,8 +1,10 @@
 package me.baraban4ik.ecolobby.listeners;
 
 import me.baraban4ik.ecolobby.managers.ActionManager;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -11,15 +13,17 @@ import java.util.List;
 import static me.baraban4ik.ecolobby.EcoLobby.config;
 
 public class WorldListener implements Listener {
-    @EventHandler
-    public void JumpIntoTheVoid (PlayerMoveEvent event) {
+    @EventHandler (priority = EventPriority.LOWEST)
+    public void JumpIntoTheVoid(PlayerMoveEvent event) {
         Player player = event.getPlayer();
 
         double height = config.getDouble("World.jump_into_the_void.height");
         List<String> actions = config.getStringList("World.jump_into_the_void.actions");
 
         if (config.getBoolean("World.jump_into_the_void.enabled")) {
-            if (event.getPlayer().getLocation().getY() <= height) {
+            if (player.getLocation().getY() <= height) {
+                player.getPassengers().stream().filter(entity -> entity.getType() == EntityType.AREA_EFFECT_CLOUD
+                        || entity.getType() == EntityType.PLAYER).forEach(player::removePassenger);
                 ActionManager.execute(player, actions);
             }
         }
