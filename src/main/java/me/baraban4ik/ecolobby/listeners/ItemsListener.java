a0e403d0bfb55f54ed36917f68a712ae7ad83ebc
Барабан4ик <vovapetrenko890@gmail.com>
2024-04-28 04:51:09 +0500

    Removed unused imports

--- src/main/java/me/baraban4ik/ecolobby/listeners/ItemsListener.java
+++ src/main/java/me/baraban4ik/ecolobby/listeners/ItemsListener.java
@@ -3,18 +3,13 @@ package me.baraban4ik.ecolobby.listeners;
 import me.baraban4ik.ecolobby.managers.ActionManager;
 import me.baraban4ik.ecolobby.managers.ItemManager;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.List;
 
-import static me.baraban4ik.ecolobby.EcoLobby.config;
 import static me.baraban4ik.ecolobby.EcoLobby.items;
 
 public class ItemsListener implements Listener {
