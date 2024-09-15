2f2c383170afb0519035a70e94a615547ed44e8b
Барабан4ик <vovapetrenko890@gmail.com>
2024-09-15 21:53:01 +0500

    added MessagesPath

--- src/main/java/me/baraban4ik/ecolobby/commands/base/BaseCommand.java
+++ src/main/java/me/baraban4ik/ecolobby/commands/base/BaseCommand.java
@@ -1,6 +1,7 @@
 package me.baraban4ik.ecolobby.commands.base;
 
 import me.baraban4ik.ecolobby.MESSAGES;
+import me.baraban4ik.ecolobby.enums.MessagesPath;
 import me.baraban4ik.ecolobby.utils.Chat;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,14 +9,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import static me.baraban4ik.ecolobby.utils.Chat.sendPathMessage;
-
 public abstract class BaseCommand extends BaseTabCompleter implements CommandExecutor {
 
     public boolean hasPermission(@NotNull CommandSender sender, @NotNull String permission) {
         if (sender.hasPermission(permission)) return true;
 
-        sendPathMessage("no_permission", sender, false);
+        Chat.sendPathMessage(MessagesPath.NO_PERMISSION.getPath(), sender);
         return false;
     }
     public boolean isPlayer(@NotNull CommandSender sender) {
