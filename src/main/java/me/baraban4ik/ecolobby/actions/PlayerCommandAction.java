c8b889d0fe0e94d240160d57ab3dec2e55af983b
Барабан4ик <vovapetrenko890@gmail.com>
2024-10-04 17:30:53 +0500

    The command to run should now be whitelisted

--- src/main/java/me/baraban4ik/ecolobby/actions/PlayerCommandAction.java
+++ src/main/java/me/baraban4ik/ecolobby/actions/PlayerCommandAction.java
@@ -1,11 +1,24 @@
 package me.baraban4ik.ecolobby.actions;
 
+import me.baraban4ik.ecolobby.enums.Path;
 import me.baraban4ik.ecolobby.models.PlayerAction;
 import org.bukkit.entity.Player;
+
+import java.util.List;
+
+import static me.baraban4ik.ecolobby.EcoLobby.config;
 
 public class PlayerCommandAction implements PlayerAction {
     @Override
     public void execute(Player player, String action) {
-        player.performCommand(action);
+        if (config.getBoolean(Path.ABILITIES_COMMANDS.getPath())) {
+            List<String> allowedCommands = config.getStringList(Path.ABILITIES_COMMANDS_ALLOWED.getPath());
+
+            if (allowedCommands.contains(action) && !(player.hasPermission("ecolobby.bypass.commands"))) {
+                player.performCommand(action);
+            }
+			return;
+        }
+		player.performCommand(action);
     }
 }
