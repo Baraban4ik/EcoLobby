7c2b39383313f00dcb69f19c0e2c7b9e057c6387
Барабан4ик <vovapetrenko890@gmail.com>
2025-01-21 22:18:30 +0500

    remove player from ParticleFallTask

--- src/main/java/me/baraban4ik/ecolobby/listeners/LeaveListeners.java
+++ src/main/java/me/baraban4ik/ecolobby/listeners/LeaveListeners.java
@@ -3,6 +3,7 @@ package me.baraban4ik.ecolobby.listeners;
 import com.xxmicloxx.NoteBlockAPI.NoteBlockAPI;
 import me.baraban4ik.ecolobby.EcoLobby;
 import me.baraban4ik.ecolobby.enums.Path;
+import me.baraban4ik.ecolobby.tasks.ParticleFallTask;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,5 +21,7 @@ public class LeaveListeners implements Listener {
             player.getInventory().clear();
         if (EcoLobby.NOTE_BLOCK_API)
             NoteBlockAPI.stopPlaying(event.getPlayer());
+
+        ParticleFallTask.remove(player);
     }
 }
