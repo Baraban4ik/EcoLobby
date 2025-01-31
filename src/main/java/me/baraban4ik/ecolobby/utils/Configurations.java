aad7b390d0050d485149c4f1539f1ae849107ba2
Барабан4ик <vovapetrenko890@gmail.com>
2025-01-31 23:15:16 +0500

    add gui configuration load

--- src/main/java/me/baraban4ik/ecolobby/utils/Configurations.java
+++ src/main/java/me/baraban4ik/ecolobby/utils/Configurations.java
@@ -7,13 +7,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static me.baraban4ik.ecolobby.EcoLobby.config;
+
 public class Configurations {
 
     private final Map<String, FileConfiguration> configurations = new HashMap<>();
+    private final Map<String, FileConfiguration> guis = new HashMap<>();
 
     private final List<String> names;
     private final JavaPlugin plugin;
@@ -38,13 +42,56 @@ public class Configurations {
         }
     }
 
+    public void loadGUIs() {
+        if (config.getBoolean("gui_example")) {
+            File gui_example = new File(plugin.getDataFolder(),  "guis/gui_example.yml");
+
+            if (!gui_example.exists()) {
+                plugin.saveResource("guis/gui_example.yml", false);
+            }
+        }
+
+        File guisFolder = new File(plugin.getDataFolder(), "guis");
+
+        if (!guisFolder.exists()) {
+            guisFolder.mkdirs();
+            return;
+        }
+        File[] files = guisFolder.listFiles((dir, name) -> name.endsWith(".yml"));
+
+        if (files != null) {
+            for (File file : files) {
+                String fileName = file.getName().replace(".yml", ""); // Убираем расширение
+                FileConfiguration configuration = YamlConfiguration.loadConfiguration(file);
+                guis.put(fileName, configuration);
+            }
+        }
+    }
+
+    public List<String> getGUIs() {
+        List<String> guiKeys = new ArrayList<>();
+        for (Map.Entry<String, FileConfiguration> entry : guis.entrySet()) {
+            guiKeys.add(entry.getKey());
+        }
+        return guiKeys;
+    }
+
+
+
     public void reload() {
         configurations.clear();
         this.load();
     }
+    public void reloadGUIs() {
+        guis.clear();
+        this.loadGUIs();
+    }
 
     public FileConfiguration getConfig(String name) {
         return configurations.get(name);
+    }
+    public FileConfiguration getGui(String name) {
+        return guis.get(name);
     }
 
     public void save(FileConfiguration fileConfiguration, String name)  {
