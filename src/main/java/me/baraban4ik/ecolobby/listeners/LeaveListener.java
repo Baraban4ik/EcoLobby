5a2ed10165a4f202c94c5aca89d30d6bc4797c6b
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:50:50 +0500

    Create LeaveListener.java

---
+++ src/main/java/me/baraban4ik/ecolobby/listeners/LeaveListener.java
@@ -0,0 +1,29 @@
+package me.baraban4ik.ecolobby.listeners;
+
+import com.xxmicloxx.NoteBlockAPI.NoteBlockAPI;
+import me.baraban4ik.ecolobby.EcoLobby;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class LeaveListener implements Listener {
+
+    private final EcoLobby plugin = EcoLobby.getInstance();
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
+        if (ConfigManager.getConfig().isLeaveClearItems())
+            player.getInventory().clear();
+
+
+        if (EcoLobby.NOTE_BLOCK_API) {
+            NoteBlockAPI.stopPlaying(event.getPlayer());
+        }
+        plugin.getDisplayManagers().forEach(d -> d.removePlayer(player));
+        plugin.getAmbientManager().removePlayer(player);
+    }
+}
