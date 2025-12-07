78641be7bf51d458b9f7ce32fcd9f328afadc075
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:45:01 +0500

    New config system

--- src/main/java/me/baraban4ik/ecolobby/actions/TeleportSpawnAction.java
+++ src/main/java/me/baraban4ik/ecolobby/actions/TeleportSpawnAction.java
@@ -1,15 +1,17 @@
 package me.baraban4ik.ecolobby.actions;
 
-import me.baraban4ik.ecolobby.enums.SpawnType;
-import me.baraban4ik.ecolobby.managers.SpawnManager;
-import me.baraban4ik.ecolobby.models.PlayerAction;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.config.files.SpawnConfig;
+import me.baraban4ik.ecolobby.enums.types.SpawnType;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class TeleportSpawnAction implements PlayerAction {
     @Override
     public void execute(Player player, String action) {
-        Location spawn = SpawnManager.getSpawn(SpawnType.MAIN);
+        SpawnConfig spawnConfig = ConfigManager.getSpawnConfig();
+
+        Location spawn = spawnConfig.getSpawn(SpawnType.MAIN);
         if (spawn != null) player.teleport(spawn);
     }
 }
