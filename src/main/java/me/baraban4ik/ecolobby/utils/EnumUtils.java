be5033b44a0eeea1fd3ef1a68009c831df3b41f2
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:51:46 +0500

    Create EnumUtils.java

---
+++ src/main/java/me/baraban4ik/ecolobby/utils/EnumUtils.java
@@ -0,0 +1,63 @@
+package me.baraban4ik.ecolobby.utils;
+
+import me.baraban4ik.ecolobby.EcoLobby;
+import org.bukkit.Sound;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EnumUtils {
+
+    private static final Set<String> loggedErrors = new HashSet<>();
+
+    @SuppressWarnings("unchecked")
+    public static <E> E parseEnum(String value, Class<E> enumClass, E defaultValue) {
+        if (enumClass == Sound.class) {
+            return (E) parseSound(value, (Sound) defaultValue);
+        }
+
+        if (Enum.class.isAssignableFrom(enumClass)) {
+            try {
+                return (E) Enum.valueOf((Class<Enum>) enumClass, value.toUpperCase());
+            } catch (Exception e) {
+                logError(enumClass, value, defaultValue);
+                return defaultValue;
+            }
+        }
+
+        logError(enumClass, value, defaultValue);
+        return defaultValue;
+    }
+
+    private static Sound parseSound(String value, Sound defaultValue) {
+        String name = value.toUpperCase();
+
+        try {
+            return Sound.valueOf(name);
+        } catch (Throwable ignored) {}
+
+        try {
+            Method fromName = Sound.class.getDeclaredMethod("fromName", String.class);
+            Object result = fromName.invoke(null, name);
+            if (result != null) {
+                return (Sound) result;
+            }
+        } catch (Throwable ignored) {}
+
+        logError(Sound.class, value, defaultValue);
+        return defaultValue;
+    }
+
+    private static void logError(Class<?> enumClass, String value, Object defaultValue) {
+        String key = enumClass.getSimpleName() + ":" + value.toUpperCase();
+
+        if (!loggedErrors.contains(key)) {
+            loggedErrors.add(key);
+            EcoLobby.getInstance().getLogger().warning(
+                    "Incorrect value '" + value + "' for " + enumClass.getSimpleName() +
+                            ", default value will be used: " + defaultValue
+            );
+        }
+    }
+}
