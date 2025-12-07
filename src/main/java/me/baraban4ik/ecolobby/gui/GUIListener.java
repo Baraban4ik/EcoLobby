b7ded12a922993960298f2c0227449b8cac949c1
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:50:08 +0500

    Update GUIListener.java

--- src/main/java/me/baraban4ik/ecolobby/gui/GUIListener.java
+++ src/main/java/me/baraban4ik/ecolobby/gui/GUIListener.java
@@ -5,12 +5,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-
-
 public class GUIListener implements Listener {
-
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
@@ -21,14 +20,21 @@ public class GUIListener implements Listener {
         Player player = (Player) event.getWhoClicked();
         ItemStack clickedItem = event.getCurrentItem();
 
-        if (clickedItem != null) {
-
-            if (ItemManager.isEcoItem(clickedItem)) {
-                GUI gui = GUIData.getGUI(event.getClickedInventory());
-                if (gui != null) gui.click(player, clickedItem);
-
-            }
+        if (ItemManager.isEcoItem(clickedItem)) {
+            GUI gui = GUIData.getGUI(event.getClickedInventory());
+            if (gui != null) gui.click(player, clickedItem);
         }
     }
 
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent event) {
+        Inventory inventory = event.getInventory();
+
+        if (GUIData.containsInventory(inventory))  {
+            GUI gui = GUIData.getGUI(inventory);
+
+            if (gui != null) gui.close();
+            GUIData.removeInventory(inventory);
+        }
+    }
 }
