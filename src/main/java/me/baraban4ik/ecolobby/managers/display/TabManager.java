c7bd9741f3ca5d0cf17273a73481c8aa1634651e
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:51:19 +0500

    Create TabManager.java

---
+++ src/main/java/me/baraban4ik/ecolobby/managers/display/TabManager.java
@@ -0,0 +1,46 @@
+package me.baraban4ik.ecolobby.managers.display;
+
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.config.files.TablistConfig;
+import me.baraban4ik.ecolobby.message.FormattedMessage;
+import org.bukkit.entity.Player;
+
+public class TabManager extends AbstractDisplayManager<Void> {
+
+    private final TablistConfig tablistConfig = ConfigManager.getTablistConfig();
+
+    @Override
+    public boolean isEnabled() {
+        return tablistConfig.isEnabled();
+    }
+
+    @Override
+    protected long getRefreshTicks() {
+        return tablistConfig.getRefresh() * 20L;
+    }
+
+    @Override
+    protected void createDisplay(Player player) {
+        addPlayer(player, null);
+    }
+
+    @Override
+    public void update(Player player) {
+        FormattedMessage header = tablistConfig.getHeader();
+        FormattedMessage footer = tablistConfig.getFooter();
+
+        FormattedMessage prefix = tablistConfig.getPrefix();
+        FormattedMessage suffix = tablistConfig.getSuffix();
+
+        player.setPlayerListName(prefix.toString(player) + player.getName() + suffix.toString(player));
+        player.setPlayerListHeader(header.toString(player));
+        player.setPlayerListFooter(footer.toString(player));
+    }
+
+    @Override
+    protected void removeDisplay(Player player) {
+        player.setPlayerListName(player.getName());
+        player.setPlayerListHeader("");
+        player.setPlayerListFooter("");
+    }
+}
