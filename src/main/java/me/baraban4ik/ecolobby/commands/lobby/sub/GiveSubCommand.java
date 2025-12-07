077b4c5bc7bfa97b80e9950b30e84f4f2cee71e4
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:46:24 +0500

    Update GiveSubCommand.java

--- src/main/java/me/baraban4ik/ecolobby/commands/lobby/sub/GiveSubCommand.java
+++ src/main/java/me/baraban4ik/ecolobby/commands/lobby/sub/GiveSubCommand.java
@@ -1,19 +1,21 @@
 package me.baraban4ik.ecolobby.commands.lobby.sub;
 
-import com.google.common.collect.Lists;
-import me.baraban4ik.ecolobby.MESSAGES;
+import me.baraban4ik.ecolobby.EcoLobby;
 import me.baraban4ik.ecolobby.commands.base.PlayerSubCommand;
+import me.baraban4ik.ecolobby.enums.Permission;
+import me.baraban4ik.ecolobby.enums.PluginMessage;
 import me.baraban4ik.ecolobby.managers.ItemManager;
-import me.baraban4ik.ecolobby.utils.Chat;
+import me.baraban4ik.ecolobby.message.PluginMessageSender;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static me.baraban4ik.ecolobby.utils.Configurations.itemsConfig;
 
 public class GiveSubCommand extends PlayerSubCommand {
 
@@ -23,60 +25,48 @@ public class GiveSubCommand extends PlayerSubCommand {
     }
 
     @Override
-    public String getPermission() {
-        return "ecolobby.command.give";
-    }
-
-    @Override
-    public boolean executePlayer(Player player, String[] args) {
-        if (args.length == 1) {
-            ItemManager.giveItem(player, args[0]);
-            Chat.sendMessage(MESSAGES.SUCCESSFULLY_GIVE_ITEM(), player);
-            return true;
-        }
-
-        sendHelp(player);
-        return true;
+    public Permission getPermission() {
+        return Permission.COMMAND_GIVE;
     }
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
         if (args.length == 2) {
             Player target = Bukkit.getPlayer(args[0]);
-            if (target == null) {
-                Chat.sendMessage(MESSAGES.PLAYER_NOT_FOUND(), sender);
-                return true;
-            }
 
-            ItemManager.giveItem(target, args[1]);
-            Chat.sendMessage(MESSAGES.SUCCESSFULLY_GIVE_ITEM(), sender);
+            if (ItemManager.giveItem(sender, target, args[1])) {
+                PluginMessageSender.send(sender, PluginMessage.SUCCESSFULLY_GIVE_ITEM);
+            }
             return true;
         }
         return super.execute(sender, args);
     }
 
+    @Override
+    public boolean executePlayer(Player player, String[] args) {
+        if (args.length == 1) {
+            if (ItemManager.giveItem(player, player, args[0])) {
+                PluginMessageSender.send(player, PluginMessage.SUCCESSFULLY_GIVE_ITEM);
+            }
+            return true;
+        }
+        sendHelp(player);
+        return true;
+    }
+
     private void sendHelp(CommandSender sender) {
-        Chat.sendMessage(MESSAGES.GIVE_USAGE(), sender);
+        PluginMessageSender.send(sender, PluginMessage.GIVE_USAGE);
     }
 
     @Override
     public List<String> tabComplete(CommandSender sender, String[] args) {
-        ConfigurationSection itemsSection = itemsConfig.getConfigurationSection("Items");
+        List<String> suggestions = Stream.concat(
+                EcoLobby.getConfigManager().getItemsNames().stream(),
+                Bukkit.getOnlinePlayers().stream().map(HumanEntity::getName)
+        ).collect(Collectors.toList());
 
-        switch (args.length) {
-            case 1:
-                if (itemsSection != null) {
-                    argsComplete.addAll(itemsSection.getKeys(false));
-                    Bukkit.getOnlinePlayers().forEach(p -> argsComplete.add(p.getName()));
-
-                    return argsComplete;
-
-                }
-                break;
-            case 2: return Lists.newArrayList(itemsSection.getKeys(false));
-        }
-
-        return Collections.emptyList();
+        return (args.length == 1 || args.length == 2) ? suggestions : Collections.emptyList();
     }
-}
 
+
+}
