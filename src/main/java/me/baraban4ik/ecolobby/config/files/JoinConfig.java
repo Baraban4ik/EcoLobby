8517347c728d14bc1b119ad26ac8aa680420bc15
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:47:54 +0500

    Create JoinConfig.java

---
+++ src/main/java/me/baraban4ik/ecolobby/config/files/JoinConfig.java
@@ -0,0 +1,38 @@
+package me.baraban4ik.ecolobby.config.files;
+
+import lombok.Getter;
+import me.baraban4ik.ecolobby.config.AbstractConfig;
+import me.baraban4ik.ecolobby.enums.paths.FilePath;
+import me.baraban4ik.ecolobby.enums.paths.JoinPath;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.util.List;
+@Getter
+public class JoinConfig extends AbstractConfig {
+
+    private boolean teleportToSpawn;
+    private List<String> actions;
+    private boolean clearChat;
+    private boolean musicEnabled;
+    private List<String> musicTracks;
+    private boolean musicRepeat;
+    private boolean musicRandom;
+
+    @Override
+    public void initialize(ConfigurationSection config) {
+        super.initialize(config);
+
+        teleportToSpawn = getBoolean(JoinPath.TELEPORT_TO_SPAWN);
+        actions = getStringList(JoinPath.ACTIONS);
+        clearChat = getBoolean(JoinPath.CLEAR_CHAT);
+        musicEnabled = getBoolean(JoinPath.MUSIC_ENABLED);
+        musicTracks = getStringList(JoinPath.MUSIC_TRACKS);
+        musicRepeat = getBoolean(JoinPath.MUSIC_REPEAT);
+        musicRandom = getBoolean(JoinPath.MUSIC_RANDOM);
+    }
+
+    @Override
+    public String getPath() {
+        return FilePath.JOIN.get();
+    }
+}
