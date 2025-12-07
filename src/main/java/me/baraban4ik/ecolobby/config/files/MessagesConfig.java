e1c0bba66b58484130dae20c906c598a0d8653fa
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:47:56 +0500

    Create MessagesConfig.java

---
+++ src/main/java/me/baraban4ik/ecolobby/config/files/MessagesConfig.java
@@ -0,0 +1,54 @@
+package me.baraban4ik.ecolobby.config.files;
+
+import lombok.Getter;
+import me.baraban4ik.ecolobby.config.AbstractConfig;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.enums.Language;
+import me.baraban4ik.ecolobby.enums.paths.FilePath;
+import me.baraban4ik.ecolobby.enums.paths.MessagesPath;
+import me.baraban4ik.ecolobby.message.FormattedMessage;
+import org.bukkit.configuration.ConfigurationSection;
+
+
+@Getter
+public class MessagesConfig extends AbstractConfig {
+
+    private String prefix;
+    private FormattedMessage noPermission;
+    private FormattedMessage whitelistKick;
+    private FormattedMessage blacklistKick;
+    private FormattedMessage denyChat;
+    private FormattedMessage denyCommands;
+    private FormattedMessage denyBreakBlocks;
+    private FormattedMessage denyPlaceBlocks;
+    private FormattedMessage denyInteractBlocks;
+    private FormattedMessage teleportedSpawn;
+    private FormattedMessage nullSpawn;
+
+    @Override
+    public void initialize(ConfigurationSection config) {
+        super.initialize(config);
+
+        prefix = config.getString(MessagesPath.PREFIX.get());
+        noPermission = getFormattedMessage(MessagesPath.NO_PERMISSION);
+
+        whitelistKick = getFormattedMessage(MessagesPath.WHITELIST_KICK);
+        blacklistKick = getFormattedMessage(MessagesPath.BLACKLIST_KICK);
+
+        denyChat = getFormattedMessage(MessagesPath.DENY_CHAT);
+        denyCommands = getFormattedMessage(MessagesPath.DENY_COMMANDS);
+
+        denyBreakBlocks = getFormattedMessage(MessagesPath.DENY_BREAK_BLOCKS);
+        denyPlaceBlocks = getFormattedMessage(MessagesPath.DENY_PLACE_BLOCKS);
+        denyInteractBlocks = getFormattedMessage(MessagesPath.DENY_INTERACT_BLOCKS);
+
+        teleportedSpawn = getFormattedMessage(MessagesPath.TELEPORTED_SPAWN);
+        nullSpawn = getFormattedMessage(MessagesPath.NULL_SPAWN);
+    }
+
+    @Override
+    public String getPath() {
+        Language language = ConfigManager.getConfig().getLanguage();
+        return FilePath.MESSAGES.get() + language.getSuffix();
+    }
+}
