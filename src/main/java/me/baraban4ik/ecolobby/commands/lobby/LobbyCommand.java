06075ebee4a243c1dfa9c90612016964f60e3de5
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:46:07 +0500

    Update LobbyCommand.java

--- src/main/java/me/baraban4ik/ecolobby/commands/lobby/LobbyCommand.java
+++ src/main/java/me/baraban4ik/ecolobby/commands/lobby/LobbyCommand.java
@@ -1,14 +1,15 @@
 package me.baraban4ik.ecolobby.commands.lobby;
 
-import me.baraban4ik.ecolobby.MESSAGES;
 import me.baraban4ik.ecolobby.commands.base.AbstractCommand;
 import me.baraban4ik.ecolobby.commands.lobby.sub.*;
-import me.baraban4ik.ecolobby.commands.lobby.sub.GiveSubCommand;
-import me.baraban4ik.ecolobby.utils.Chat;
+import me.baraban4ik.ecolobby.commands.lobby.sub.spawn.SetSpawnSubCommand;
+import me.baraban4ik.ecolobby.commands.lobby.sub.spawn.SpawnSubCommand;
+import me.baraban4ik.ecolobby.enums.Permission;
+import me.baraban4ik.ecolobby.enums.PluginMessage;
+import me.baraban4ik.ecolobby.message.PluginMessageSender;
 import org.bukkit.command.CommandSender;
 
 public class LobbyCommand extends AbstractCommand {
-
 
     public LobbyCommand() {
         registerSubCommand(new ReloadSubCommand());
@@ -16,13 +17,12 @@ public class LobbyCommand extends AbstractCommand {
         registerSubCommand(new SpawnSubCommand());
         registerSubCommand(new GiveSubCommand());
         registerSubCommand(new OpenSubCommand());
-        registerSubCommand(new HelpSubCommand());
     }
 
     @Override
     protected void sendHelp(CommandSender sender) {
-        if (hasPermission(sender, "ecolobby.command.help")) {
-            Chat.sendMessage(MESSAGES.HELP_MESSAGE(), sender);
+        if (Permission.COMMAND.has(sender, true)) {
+            PluginMessageSender.send(sender, PluginMessage.HELP_MESSAGE);
         }
     }
 }
