58e6fa429fea06e7181bcd193d4c81120587a3b3
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:51:18 +0500

    Create ScoreboardManager.java

---
+++ src/main/java/me/baraban4ik/ecolobby/managers/display/ScoreboardManager.java
@@ -0,0 +1,45 @@
+package me.baraban4ik.ecolobby.managers.display;
+
+import fr.mrmicky.fastboard.FastBoard;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.config.files.ScoreboardConfig;
+import me.baraban4ik.ecolobby.message.FormattedMessage;
+import org.bukkit.entity.Player;
+
+public class ScoreboardManager extends AbstractDisplayManager<FastBoard> {
+
+    private final ScoreboardConfig scoreboardConfig = ConfigManager.getScoreboardConfig();
+
+    @Override
+    public boolean isEnabled() {
+        return scoreboardConfig.isEnabled();
+    }
+
+    @Override
+    protected long getRefreshTicks() {
+        return scoreboardConfig.getRefresh() * 20L;
+    }
+
+    @Override
+    protected void createDisplay(Player player) {
+        FastBoard board = new FastBoard(player);
+        addPlayer(player, board);
+    }
+
+    @Override
+    public void update(Player player) {
+        FastBoard board = playerDisplays.get(player.getUniqueId());
+        if (board == null) return;
+
+        FormattedMessage title = scoreboardConfig.getTitle();
+        FormattedMessage lines = scoreboardConfig.getLines();
+
+        board.updateTitle(title.toString(player));
+        board.updateLines(lines.toList(player));
+    }
+
+    @Override
+    protected void removeDisplay(FastBoard display) {
+        display.delete();
+    }
+}
