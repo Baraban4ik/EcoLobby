ed7ac9c153fc68012c4354c75beb3b45c195385c
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:48:36 +0500

    Create ConfigPath.java

---
+++ src/main/java/me/baraban4ik/ecolobby/enums/paths/ConfigPath.java
@@ -0,0 +1,31 @@
+package me.baraban4ik.ecolobby.enums.paths;
+
+public enum ConfigPath implements IPath{
+
+    CHECK_UPDATES("check_updates"),
+    LANGUAGE("language"),
+
+    WHITELIST_ENABLED("Whitelist.enabled"),
+    WHITELIST_PLAYERS("Whitelist.players"),
+
+    BLACKLIST_ENABLED("Blacklist.enabled"),
+    BLACKLIST_PLAYERS("Blacklist.players"),
+
+    LEAVE_CLEAR_ITEMS("Leave.clear_items"),
+    HIDE_STREAM_ALL("Hiders.hide_stream.all"),
+    HIDE_STREAM_JOIN("Hiders.hide_stream.join"),
+    HIDE_STREAM_LEAVE("Hiders.hide_stream.leave"),
+    HIDE_STREAM_DEATH("Hiders.hide_stream.death"),
+    HIDE_STREAM_KICK("Hiders.hide_stream.kick"),
+    HIDE_PLAYERS("Hiders.hide_stream.players");
+
+    private final String path;
+
+    ConfigPath(String path) {
+        this.path = path;
+    }
+    @Override
+    public String get() {
+        return path;
+    }
+}
