d134e710d684005125b6d166aa440ba869f37851
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:51:07 +0500

    Update WorldListener.java

--- src/main/java/me/baraban4ik/ecolobby/listeners/WorldListener.java
+++ src/main/java/me/baraban4ik/ecolobby/listeners/WorldListener.java
@@ -1,34 +1,14 @@
 package me.baraban4ik.ecolobby.listeners;
 
-import me.baraban4ik.ecolobby.enums.Path;
-import me.baraban4ik.ecolobby.managers.ActionManager;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import me.baraban4ik.ecolobby.EcoLobby;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
-
-import java.util.List;
-
-import static me.baraban4ik.ecolobby.utils.Configurations.config;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class WorldListener implements Listener {
-    @EventHandler (priority = EventPriority.LOWEST)
-    public void onJumpVoid(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
 
-        double height = config.getDouble(Path.VOID_HEIGHT.getPath());
-        List<String> actions = config.getStringList(Path.VOID_ACTIONS.getPath());
-
-        if (config.getBoolean(Path.VOID.getPath())) {
-            if (player.getLocation().getY() <= height) {
-                player.getPassengers().stream().filter(entity -> entity.getType() == EntityType.AREA_EFFECT_CLOUD
-                        || entity.getType() == EntityType.PLAYER).forEach(player::removePassenger);
-
-                ActionManager.execute(player, actions);
-            }
-        }
+    @EventHandler
+    public void onWorldChange(PlayerChangedWorldEvent event) {
+        EcoLobby.getInstance().getAmbientManager().update(event.getPlayer());
     }
 }
-
