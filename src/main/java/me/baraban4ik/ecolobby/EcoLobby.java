db4938bedb71a9692cef1c664910a12b4fc7dc3e
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:48:23 +0500

    Update EcoLobby.java

--- src/main/java/me/baraban4ik/ecolobby/EcoLobby.java
+++ src/main/java/me/baraban4ik/ecolobby/EcoLobby.java
@@ -1,35 +1,35 @@
 package me.baraban4ik.ecolobby;
 
+import lombok.Getter;
 import me.baraban4ik.ecolobby.commands.lobby.LobbyCommand;
 import me.baraban4ik.ecolobby.commands.spawn.SetSpawnCommand;
 import me.baraban4ik.ecolobby.commands.spawn.SpawnCommand;
-import me.baraban4ik.ecolobby.enums.Path;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.enums.PluginMessage;
 import me.baraban4ik.ecolobby.gui.GUI;
 import me.baraban4ik.ecolobby.gui.GUIData;
 import me.baraban4ik.ecolobby.gui.GUIListener;
 import me.baraban4ik.ecolobby.listeners.*;
-import me.baraban4ik.ecolobby.managers.BossBarManager;
-import me.baraban4ik.ecolobby.managers.ScoreBoardManager;
-import me.baraban4ik.ecolobby.managers.TabManager;
-import me.baraban4ik.ecolobby.tasks.ParticleFallTask;
-import me.baraban4ik.ecolobby.utils.Chat;
-import me.baraban4ik.ecolobby.utils.Configurations;
-import me.baraban4ik.ecolobby.utils.Update;
+import me.baraban4ik.ecolobby.managers.AmbientManager;
+import me.baraban4ik.ecolobby.managers.WorldsManager;
+import me.baraban4ik.ecolobby.managers.display.AbstractDisplayManager;
+import me.baraban4ik.ecolobby.managers.display.BossbarManager;
+import me.baraban4ik.ecolobby.managers.display.ScoreboardManager;
+import me.baraban4ik.ecolobby.managers.display.TabManager;
+import me.baraban4ik.ecolobby.message.PluginMessageSender;
+import me.baraban4ik.ecolobby.utils.UpdateChecker;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
-import org.bukkit.GameRule;
-import org.bukkit.World;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static me.baraban4ik.ecolobby.utils.Chat.*;
-import static me.baraban4ik.ecolobby.utils.Configurations.*;
+import java.util.*;
 
-public class EcoLobby extends JavaPlugin {
-
-    private static Configurations configurations;
-    private static EcoLobby instance;
+public final class EcoLobby extends JavaPlugin {
 
     public static boolean PLACEHOLDER_API = false;
     public static boolean NOTE_BLOCK_API = false;
@@ -37,139 +37,124 @@ public class EcoLobby extends JavaPlugin {
     public static boolean UPDATE_AVAILABLE = false;
     public static String UPDATE_VERSION = "";
 
-    private final TabManager tabManager = new TabManager();
-    private final BossBarManager barManager = new BossBarManager();
-    private final ScoreBoardManager scoreBoardManager = new ScoreBoardManager();
+    @Getter private static EcoLobby instance;
+    @Getter private static ConfigManager configManager;
 
+    @Getter private Set<AbstractDisplayManager<?>> displayManagers;
+    @Getter private AmbientManager ambientManager;
 
-    @Override
-    public void onLoad() {
-        instance = this;
-        configurations = new Configurations(this,
-                "config",
-                "language/messages",
-                "language/messages_ru",
-                "spawn",
-                "tablist",
-                "bossbar",
-                "scoreboard",
-                "items"
-        );
-    }
+    private BukkitAudiences audiences;
 
     @Override
     public void onEnable() {
-        if (config.getBoolean(Path.CHECK_UPDATES.getPath())) this.checkUpdate();
+        instance = this;
+        audiences = BukkitAudiences.create(this);
+
+        configManager = new ConfigManager(this);
+        displayManagers = new HashSet<>(Arrays.asList(
+                new TabManager(),
+                new BossbarManager(),
+                new ScoreboardManager()
+        ));
+        ambientManager = new AmbientManager();
+        ambientManager.start();
+
+        WorldsManager.apply();
+        refreshDisplays();
+
+        if (ConfigManager.getConfig().isCheckUpdates()) this.checkUpdate();
 
         PLACEHOLDER_API = getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
         NOTE_BLOCK_API = getServer().getPluginManager().isPluginEnabled("NoteBlockAPI");
 
-        this.registerCommands();
-        this.registerListeners();
-
         new Metrics(this, 14978);
         getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
 
-        if (tablistConfig.getBoolean(Path.TABLIST.getPath())) tabManager.sendTablist();
+        registerCommands();
+        registerListeners();
 
-        if (config.getBoolean(Path.FALL_PARTICLES.getPath())) {
-            Bukkit.getOnlinePlayers().forEach(ParticleFallTask::add);
-            (new ParticleFallTask()).runTaskTimer(this, 0L, 1L);
-        }
-        updateWorld();
-        Chat.sendMessage(MESSAGES.ENABLE_MESSAGE(), Bukkit.getConsoleSender());
+        PluginMessageSender.console(PluginMessage.ENABLE_MESSAGE);
     }
 
     @Override
     public void onDisable() {
-        barManager.removeAllPlayers();
-        scoreBoardManager.removeAllPlayers();
+        if (audiences != null) audiences.close();
+
+        displayManagers.forEach(AbstractDisplayManager::stop);
+        ambientManager.stop();
     }
 
-    public static EcoLobby getInstance() {
-        return instance;
+    public void reload() {
+        configManager.reload();
+
+        WorldsManager.apply();
+        refreshDisplays();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            GUI gui = GUIData.getGUI(player.getOpenInventory().getTopInventory());
+            if (gui != null) gui.update(player);
+        }
     }
-    public static Configurations getConfigurations() {
-        return configurations;
+
+    private void refreshDisplays() {
+        for (AbstractDisplayManager<?> displayManager : displayManagers) {
+            if (displayManager.isEnabled()) {
+                displayManager.start();
+                Bukkit.getOnlinePlayers().forEach(displayManager::send);
+            }
+            else displayManager.stop();
+        }
     }
 
     private void registerCommands() {
         getServer().getPluginCommand("ecolobby").setExecutor(new LobbyCommand());
-        getServer().getPluginCommand("setspawn").setExecutor(new SetSpawnCommand());
         getServer().getPluginCommand("spawn").setExecutor(new SpawnCommand());
+        getServer().getPluginCommand("setspawn").setExecutor(new SetSpawnCommand());
     }
+
     private void registerListeners() {
-        getServer().getPluginManager().registerEvents(new HiderListener(), this);
-        getServer().getPluginManager().registerEvents(new ItemsListener(), this);
-        getServer().getPluginManager().registerEvents(new JoinListener(), this);
-        getServer().getPluginManager().registerEvents(new LeaveListeners(), this);
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
-        getServer().getPluginManager().registerEvents(new PreJoinListener(), this);
-        getServer().getPluginManager().registerEvents(new WorldListener(), this);
-        getServer().getPluginManager().registerEvents(new GUIListener(), this);
-    }
+        List<Listener> listeners = Arrays.asList(
+                new BlockListener(),
+                new ChatListener(),
+                new CommandListener(),
+                new DamageListener(),
+                new HiderListener(),
+                new ItemListener(),
+                new JoinListener(),
+                new LeaveListener(),
+                new MovementListener(),
+                new PreJoinListener(),
+                new WorldListener(),
+                new GUIListener()
 
-    private void updateWorld() {
-        for (World world : Bukkit.getWorlds()) {
-            ConfigurationSection rules = config.getConfigurationSection(Path.WORLD_RULES.getPath());
+        );
 
-            if (rules == null) return;
-            long time = config.getLong(Path.WORLD_TIME.getPath());
-
-            for (String rule : rules.getKeys(false)) {
-                GameRule<?> gameRule = GameRule.getByName(rule);
-
-                if (gameRule != null) {
-                    String value = rules.getString(rule);
-                    Object parsedValue;
-
-                    if (gameRule.getType() == Boolean.class) {
-                        parsedValue = Boolean.parseBoolean(value);
-                    }
-                    else if (gameRule.getType() == Integer.class) {
-                        parsedValue = Integer.parseInt(value);
-                    }
-                    else continue;
-
-                    world.setGameRule((GameRule<Object>) gameRule, parsedValue);
-                }
-            }
-            world.setTime(time);
-        }
+        listeners.forEach(listener ->
+            getServer().getPluginManager().registerEvents(listener, this)
+        );
     }
 
     private void checkUpdate() {
-        new Update().getVersion(version ->
+        new UpdateChecker().getVersion(version ->
         {
             if (!this.getDescription().getVersion().equals(version)) {
                 UPDATE_AVAILABLE = true;
                 UPDATE_VERSION = version;
 
-                sendMessage(MESSAGES.NEW_VERSION(UPDATE_VERSION), Bukkit.getConsoleSender());
+                PluginMessageSender.sendUpdateMessage(Bukkit.getConsoleSender());
             }
         });
+
     }
-
-    public void reload() {
-        configurations.reload();
-
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            if (tablistConfig.getBoolean(Path.TABLIST.getPath())) tabManager.update(player);
-            if (bossBarConfig.getBoolean(Path.BOSSBAR.getPath())) barManager.update(player);
-            if (scoreboardConfig.getBoolean(Path.SCOREBOARD.getPath())) scoreBoardManager.update(player);
-
-            GUI gui = GUIData.getGUI(player.getOpenInventory().getTopInventory());
-            if (gui != null) gui.update(player);
-
-        }
-        updateWorld();
-    }
-
 
     public Float getServerVersion() {
         String version = Bukkit.getBukkitVersion().split("-")[0];
         String[] versionParts = version.split("\\.");
 
         return Float.parseFloat(versionParts[0] + "." + versionParts[1] + versionParts[2]);
+    }
+
+    public Audience getAudience(CommandSender sender) {
+        return audiences.sender(sender);
     }
 }
