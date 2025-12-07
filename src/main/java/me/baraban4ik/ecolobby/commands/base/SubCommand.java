d666e28986bce5427e50f1446599ae29615ab938
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:46:05 +0500

    Update SubCommand.java

--- src/main/java/me/baraban4ik/ecolobby/commands/base/SubCommand.java
+++ src/main/java/me/baraban4ik/ecolobby/commands/base/SubCommand.java
@@ -1,15 +1,14 @@
 package me.baraban4ik.ecolobby.commands.base;
 
+import me.baraban4ik.ecolobby.enums.Permission;
 import org.bukkit.command.CommandSender;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface SubCommand {
     String getName();
-    String getPermission();
+    Permission getPermission();
     boolean execute(CommandSender sender, String[] args);
 
-    List<String> argsComplete = new ArrayList<>();
     List<String> tabComplete(CommandSender sender, String[] args);
 }
