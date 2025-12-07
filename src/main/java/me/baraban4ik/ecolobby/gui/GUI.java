f1a767b7e00fd963dda49372cc8faefab9d219bc
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:49:28 +0500

    Update GUI.java

--- src/main/java/me/baraban4ik/ecolobby/gui/GUI.java
+++ src/main/java/me/baraban4ik/ecolobby/gui/GUI.java
@@ -1,140 +1,114 @@
 package me.baraban4ik.ecolobby.gui;
 
+
 import me.baraban4ik.ecolobby.EcoLobby;
-import me.baraban4ik.ecolobby.enums.Path;
+import me.baraban4ik.ecolobby.config.ConfigManager;
+import me.baraban4ik.ecolobby.config.files.GUIConfig;
+import me.baraban4ik.ecolobby.config.files.ItemConfig;
 import me.baraban4ik.ecolobby.managers.ActionManager;
 import me.baraban4ik.ecolobby.managers.ItemManager;
-import me.baraban4ik.ecolobby.utils.Format;
 import org.bukkit.Bukkit;
 
-import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class GUI {
-
-    private final List<Integer> refreshableSlots = new ArrayList<>();
-
-
-    private int size = 54;
-    private String title;
-
-    private ConfigurationSection items;
-    private Inventory gui;
-    public boolean isActive = true;
-
+    private final ConfigManager configManager;
     private final String name;
 
+    private GUIConfig guiConfig;
+    private ConfigurationSection items;
+
+    private Inventory gui;
+    private BukkitTask refreshTask;
+
+    public boolean isLoaded;
+
     public GUI(String name) {
+        this.configManager = EcoLobby.getConfigManager();
         this.name = name;
+
         loadData();
     }
-    private void loadData() {
-        FileConfiguration guiConfig = EcoLobby.getConfigurations().getGui(name);
-        if (guiConfig == null) {
-            isActive = false;
-            return;
-        }
 
-        title = guiConfig.getString(Path.GUI_TITLE.getPath());
-        size = guiConfig.getInt(Path.GUI_SIZE.getPath());
-        items = guiConfig.getConfigurationSection(Path.ITEMS.getPath());
+    private void loadData() {
+        guiConfig = configManager.getGUIConfig(name);
+        isLoaded = guiConfig != null;
     }
 
-
     public void open(Player player) {
-        if (!isActive) return;
+        if (!isLoaded) return;
 
-        gui = Bukkit.createInventory(null, size, Format.format(title, player));
-        addItems(player);
+        String title = guiConfig.getTitle().toString(player);
+        int size = guiConfig.getSize();
+
+        items = guiConfig.getItems();
+        gui = Bukkit.createInventory(null, size, title);
+
+        fillItems(player, false);
 
         GUIData.addInventory(gui, this);
         player.openInventory(gui);
+
         refresh(player);
     }
 
+    public void close() {
+        if (refreshTask != null) refreshTask.cancel();
+    }
 
     public void update(Player player) {
         loadData();
         open(player);
     }
 
+    private void fillItems(Player player, boolean refreshOnly) {
+        if (items == null) return;
 
-    private void addItems(Player player) {
-        if (items != null) {
-            for (String itemID : items.getKeys(false)) {
-                ConfigurationSection itemIDSection = items.getConfigurationSection(itemID);
+        for (String itemID : items.getKeys(false)) {
+            ConfigurationSection itemIDSection = items.getConfigurationSection(itemID);
+            if (itemIDSection == null) continue;
 
-                if (itemIDSection != null) {
-                    ItemStack item = ItemManager.createItem(player, itemID, itemIDSection);
-                    addItemToSlots(item, itemIDSection);
+            ItemConfig itemConfig = configManager.getItemConfig(itemIDSection);
+            if (refreshOnly && !itemConfig.isRefresh()) continue;
 
-                    if (itemIDSection.getBoolean(Path.ITEM_REFRESH.getPath(), false)) {
-                        List<Integer> slots = itemIDSection.getIntegerList(Path.ITEM_SLOTS.getPath());
+            ItemStack item = ItemManager.createItem(player, itemConfig, itemID);
 
-                        if (!slots.isEmpty()) refreshableSlots.addAll(slots);
-                        else refreshableSlots.add(itemIDSection.getInt(Path.ITEM_SLOT.getPath()));
-
-                    }
-                }
-            }
+            List<Integer> slots = itemConfig.getSlots();
+            slots.forEach(s -> gui.setItem(s, item));
         }
     }
 
-    private void addItemToSlots(ItemStack item, ConfigurationSection itemID) {
-        List<Integer> itemSlots = itemID.getIntegerList(Path.ITEM_SLOTS.getPath());
-
-        if (!itemSlots.isEmpty()) itemSlots.forEach(slot -> gui.setItem(slot, item));
-        else gui.setItem(itemID.getInt(Path.ITEM_SLOT.getPath()), item);
-
-    }
-
-
     public void click(Player player, ItemStack item) {
-        PersistentDataContainer pdc = item.getItemMeta().getPersistentDataContainer();
-        NamespacedKey ECO_ITEM = new NamespacedKey(EcoLobby.getInstance(), "ECO_ITEM");
+        String itemID = ItemManager.getItemId(item);
+        if (itemID == null) return;
 
-        if (!pdc.has(ECO_ITEM, PersistentDataType.STRING)) return;
+        ConfigurationSection itemIDSection = items.getConfigurationSection(itemID);
 
-        String itemID = pdc.get(ECO_ITEM, PersistentDataType.STRING);
-        ConfigurationSection itemSection = items.getConfigurationSection(itemID);
-        if (itemSection == null) return;
+        if (itemIDSection != null) {
+            ItemConfig config = configManager.getItemConfig(itemIDSection);
+            List<String> actions = config.getActions();
 
-        List<String> actions = itemSection.getStringList(Path.ITEM_ACTIONS.getPath());
-        ActionManager.execute(player, actions);
+            ActionManager.execute(player, actions);
+        }
     }
 
     private void refresh(Player player) {
-        new BukkitRunnable() {
+        refreshTask = new BukkitRunnable() {
             @Override
             public void run() {
                 if (gui == null || !player.getOpenInventory().getTopInventory().equals(gui)) {
                     cancel();
                     return;
                 }
-                if (items != null) {
-                    for (String itemID : items.getKeys(false)) {
-                        ConfigurationSection itemIDSection = items.getConfigurationSection(itemID);
+                fillItems(player, true);
 
-                        if (itemIDSection != null && itemIDSection.getBoolean(Path.ITEM_REFRESH.getPath(), false)) {
-                            ItemStack item = ItemManager.createItem(player, itemID, itemIDSection);
-                            List<Integer> slots = itemIDSection.getIntegerList(Path.ITEM_SLOTS.getPath());
-
-                            if (!slots.isEmpty()) slots.forEach(slot -> gui.setItem(slot, item));
-                            else gui.setItem(itemIDSection.getInt(Path.ITEM_SLOT.getPath()), item);
-
-                        }
-                    }
-                }
             }
         }.runTaskTimer(EcoLobby.getInstance(), 0L, 20L);
     }
