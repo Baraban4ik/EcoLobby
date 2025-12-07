0d1d9b2b556e939494de455b127b5699fa23bfb0
Барабан4ик <vovapetrenko890@gmail.com>
2025-12-07 14:46:34 +0500

    Update OpenSubCommand.java

--- src/main/java/me/baraban4ik/ecolobby/commands/lobby/sub/OpenSubCommand.java
+++ src/main/java/me/baraban4ik/ecolobby/commands/lobby/sub/OpenSubCommand.java
@@ -1,20 +1,23 @@
 package me.baraban4ik.ecolobby.commands.lobby.sub;
 
+
 import me.baraban4ik.ecolobby.EcoLobby;
-import me.baraban4ik.ecolobby.MESSAGES;
 import me.baraban4ik.ecolobby.commands.base.PlayerSubCommand;
+import me.baraban4ik.ecolobby.enums.Permission;
+import me.baraban4ik.ecolobby.enums.PluginMessage;
 import me.baraban4ik.ecolobby.gui.GUI;
-import me.baraban4ik.ecolobby.utils.Chat;
+import me.baraban4ik.ecolobby.message.PluginMessageSender;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OpenSubCommand extends PlayerSubCommand {
-
 
     @Override
     public String getName() {
@@ -22,22 +25,23 @@ public class OpenSubCommand extends PlayerSubCommand {
     }
 
     @Override
-    public String getPermission() {
-        return "ecolobby.command.open";
+    public Permission getPermission() {
+        return Permission.COMMAND_OPEN;
     }
     @Override
     public boolean executePlayer(Player player, String[] args) {
         if (args.length == 1) {
             GUI GUI = new GUI(args[0]);
-            if (!GUI.isActive) {
-                Chat.sendMessage(MESSAGES.GUI_NOT_FOUND(), player);
+
+            if (!GUI.isLoaded) {
+                PluginMessageSender.send(player, PluginMessage.GUI_NOT_FOUND);
                 return true;
             }
+
             GUI.open(player);
-            Chat.sendMessage(MESSAGES.SUCCESSFULLY_OPEN_MENU(), player);
+            PluginMessageSender.send(player, PluginMessage.SUCCESSFULLY_OPEN_MENU);
             return true;
         }
-
         sendHelp(player);
         return true;
     }
@@ -46,31 +50,34 @@ public class OpenSubCommand extends PlayerSubCommand {
     public boolean execute(CommandSender sender, String[] args) {
         if (args.length == 2) {
             Player target = Bukkit.getPlayer(args[1]);
+
             if (target == null) {
-                Chat.sendMessage(MESSAGES.PLAYER_NOT_FOUND(), sender);
+                PluginMessageSender.send(sender, PluginMessage.PLAYER_NOT_FOUND);
                 return true;
             }
+
             GUI GUI = new GUI(args[0]);
             GUI.open(target);
 
-            Chat.sendMessage(MESSAGES.SUCCESSFULLY_OPEN_MENU(), sender);
+            PluginMessageSender.send(sender, PluginMessage.SUCCESSFULLY_OPEN_MENU);
             return true;
         }
         return super.execute(sender, args);
     }
 
     private void sendHelp(CommandSender sender) {
-        Chat.sendMessage(MESSAGES.OPEN_USAGE(), sender);
+        PluginMessageSender.send(sender, PluginMessage.OPEN_USAGE);
     }
 
     @Override
     public List<String> tabComplete(CommandSender sender, String[] args) {
+        List<String> playersNames = Bukkit.getOnlinePlayers().stream()
+                .map(HumanEntity::getName)
+                .collect(Collectors.toList());
+
         switch (args.length) {
-            case 1:
-                return EcoLobby.getConfigurations().getGUIs();
-            case 2:
-                Bukkit.getOnlinePlayers().forEach(p -> argsComplete.add(p.getName()));
-                return argsComplete;
+            case 1: return EcoLobby.getConfigManager().getGUIsNames();
+            case 2: return playersNames;
         }
         return Collections.emptyList();
     }
