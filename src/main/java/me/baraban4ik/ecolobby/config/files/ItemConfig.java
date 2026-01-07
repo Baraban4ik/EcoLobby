7e9cf4d4c52aa711e5514f3db997ea450c5a28b4
Барабан4ик <vovapetrenko890@gmail.com>
2026-01-08 00:33:57 +0500

    Added Custom Map and Equipment Slots

--- src/main/java/me/baraban4ik/ecolobby/config/files/ItemConfig.java
+++ src/main/java/me/baraban4ik/ecolobby/config/files/ItemConfig.java
@@ -4,16 +4,14 @@ import lombok.Getter;
 import me.baraban4ik.ecolobby.config.AbstractConfig;
 import me.baraban4ik.ecolobby.enums.paths.ItemPath;
 import me.baraban4ik.ecolobby.message.FormattedMessage;
-import me.baraban4ik.ecolobby.utils.EnumUtils;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemFlag;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ItemConfig extends AbstractConfig {
 
@@ -22,6 +20,7 @@ public class ItemConfig extends AbstractConfig {
     @Getter private FormattedMessage name;
     @Getter private String headName;
     @Getter private String base64Value;
+    @Getter private String customMapPath;
 
     @Getter private FormattedMessage lore;
     @Getter private List<String> actions;
@@ -30,6 +29,7 @@ public class ItemConfig extends AbstractConfig {
     @Getter private boolean refresh;
     @Getter private boolean head;
     @Getter private boolean base64;
+    @Getter private boolean customMap;
 
     @Getter private int amount;
     @Getter private int modelData;
@@ -43,32 +43,61 @@ public class ItemConfig extends AbstractConfig {
         materialName = getString(ItemPath.MATERIAL, Material.STONE.name());
 
         head = materialName.startsWith("head-");
+        headName = head ? materialName.split("head-")[1] : "";
+
         base64 = materialName.startsWith("basehead-");
+        base64Value =  base64 ? materialName.split("basehead-")[1] : "";
+
+        customMap = materialName.startsWith("map-");
+        customMapPath = customMap ? materialName.split("map-")[1] : "";
 
         name = getFormattedMessage(ItemPath.NAME);
         lore = getFormattedMessageList(ItemPath.LORE);
-        headName = head ? materialName.split("head-")[1] : "";
-        base64Value =  base64 ? materialName.split("basehead-")[1] : "";
+
         joinGive = getBoolean(ItemPath.JOIN_GIVE, true);
         refresh = getBoolean(ItemPath.REFRESH);
+
         amount = getInt(ItemPath.AMOUNT, 1);;
         modelData = getInt(ItemPath.MODEL_DATA, 0);
         actions = getStringList(ItemPath.ACTIONS);
-
     }
 
     public Material getMaterial() {
-        if (head || base64) {
+        if (head || base64)
             return Material.PLAYER_HEAD;
+        else if (customMap)
+            return Material.FILLED_MAP;
+
+        return getEnum(materialName, Material.class, Material.STONE);
+    }
+
+    public List<EquipmentSlot> getEquipmentSlots() {
+        List<EquipmentSlot> slots = getStringList(ItemPath.SLOTS).stream()
+                .filter(slot -> !isInteger(slot))
+                .map(slot -> getEnum(slot, EquipmentSlot.class))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        String slot = getString(ItemPath.SLOT);
+
+        if (slots.isEmpty() && slot != null && !isInteger(slot)) {
+            EquipmentSlot equipmentSlot = getEnum(slot, EquipmentSlot.class);
+
+            if (equipmentSlot != null)
+                slots.add(equipmentSlot);
         }
-        return EnumUtils.parseEnum(materialName, Material.class, Material.STONE);
+        return slots;
     }
 
     public List<Integer> getSlots() {
-        int slot = getInt(ItemPath.SLOT, 0);
+        int slot = getInt(ItemPath.SLOT, -1);
 
-        List<Integer> slots = getIntList(ItemPath.SLOTS);
-        if (slots.isEmpty()) slots.add(slot);
+        List<Integer> slots = getStringList(ItemPath.SLOTS).stream()
+                .filter(this::isInteger)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        if (slots.isEmpty() && slot != -1) slots.add(slot);
 
         return slots;
     }
@@ -100,5 +129,15 @@ public class ItemConfig extends AbstractConfig {
         });
 
         return flags;
+    }
+
+    private boolean isInteger(String str) {
+        if (str == null) return false;
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 }
