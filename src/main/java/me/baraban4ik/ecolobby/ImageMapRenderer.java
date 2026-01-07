27075fe634168e37946207e84785670cbb32fcda
Барабан4ик <vovapetrenko890@gmail.com>
2026-01-08 00:32:39 +0500

    Create ImageMapRenderer

---
+++ src/main/java/me/baraban4ik/ecolobby/ImageMapRenderer.java
@@ -0,0 +1,38 @@
+package me.baraban4ik.ecolobby;
+
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapCanvas;
+import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
+import org.jetbrains.annotations.NotNull;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageMapRenderer extends MapRenderer {
+
+    private final BufferedImage image;
+    private boolean rendered = false;
+
+    public ImageMapRenderer(String path) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(EcoLobby.getInstance().getDataFolder(), path));
+        } catch (IOException e) {
+            EcoLobby.getInstance().getLogger().warning(
+                    "File not found: " + path);
+        }
+        this.image = img;
+    }
+
+    @Override
+    public void render(@NotNull MapView map, @NotNull MapCanvas canvas, @NotNull Player player) {
+        if (rendered || image == null) return;
+
+        canvas.drawImage(0, 0, image);
+        rendered = true;
+    }
+}
+
